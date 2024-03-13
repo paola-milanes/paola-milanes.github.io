175a86c7efb500f4dd9e972c10fcef03382e78d9
paola-milanes <117133355+paola-milanes@users.noreply.github.com>
2024-03-12 17:29:39 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+new Typewriter("#web-developer", {
+  strings: "a web developer based in California",
+  autoStart: true,
+  delay: 80,
+  cursor: "_",
+  loop: true,
+  deleteSpeed: 30,
+});
+
+new Typewriter("#book-title", {
+  strings: "A little bit about me",
+  autoStart: true,
+  delay: 80,
+  cursor: "_",
+  loop: true,
+  deleteSpeed: 30,
+});
